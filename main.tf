7c0fb77f0dc367c9a99d9e4f7ee73bc2debdb24b
scwluaHC <shawn.lua@hashicorp.com>
2022-07-04 17:22:35 +0800

    Update main.tf

--- main.tf
+++ main.tf
@@ -127,13 +127,13 @@ resource "aws_security_group" "alb" {
     from_port   = 443
     to_port     = 443
     protocol    = "tcp"
-    cidr_blocks = var.allowed_cidr_blocks
+    cidr_blocks = ["0.0.0.0/0"]
   }
   ingress {
     from_port   = 80
     to_port     = 80
     protocol    = "tcp"
-    cidr_blocks = var.allowed_cidr_blocks
+    cidr_blocks = ["0.0.0.0/0"]
   }
   # Allow all outbound traffic
   egress {
